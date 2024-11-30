406c3cf757480993f9ba868a3cf82f7679dbfd4b
christianokuru <okuruchristian@gmail.com>
2024-11-30 09:24:11 +0100

    Shadcn and tailwind installed successfully

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+ <h1 class="text-pink-600">App pagess</h1>
+
+  <RouterView />
+</template>
+
+
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <main>
+    <h2 class="text-blue-800 font-bold text-4xl">Home</h2>
+  </main>
+</template>
