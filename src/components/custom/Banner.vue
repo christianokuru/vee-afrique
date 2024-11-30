e054bae0ab8506b31f8fe2535e388d27925467f0
christianokuru <okuruchristian@gmail.com>
2024-11-30 16:50:29 +0100

    Finished the banner section

--- src/components/custom/Banner.vue
+++ src/components/custom/Banner.vue
@@ -1,9 +1,10 @@
 <script setup>
-import banner from '@/assets/pics/banner-white.jpg'
+import banner from '@/assets/pics/banner-white.jpg';
+import banna from '@/assets/pics/banner-2.jpg';
 </script>
 
 <template>
- <section class="relative bg-gray-100">
+  <section class="relative bg-gray-200">
     <!-- Mobile View -->
     <div class="block md:hidden">
       <div class="relative">
@@ -21,20 +22,22 @@ import banner from '@/assets/pics/banner-white.jpg'
     </div>
 
     <!-- Tablet/Desktop View -->
-    <div class="hidden md:flex items-center justify-between px-10 lg:px-20 h-96">
+    <div class="hidden md:flex items-center justify-between px-32 pr-3 lg:px-32 h-auto">
       <!-- Text Section -->
-      <div class="max-w-md space-y-4">
-        <h1 class="text-4xl font-bold text-yellow-600">Welcome to Vee Afrique</h1>
-        <p class="text-lg text-gray-700">
+      <div class="w-1/2 max-w-lg space-y-6">
+        <h1 class="lg:text-5xl md:text-4xl font-bold text-yellow-600 md:w-72 lg:w-full">Welcome to Vee Afrique</h1>
+        <p class="text-xl text-gray-700 md:w-96 lg:w-full">
           Discover unique and modern designs that bring your vision to life.
         </p>
-        <button class="bg-yellow-500 text-black font-semibold py-2 px-4 rounded hover:bg-yellow-600">
+        <button class="bg-yellow-500 text-black font-semibold py-3 px-6 rounded hover:bg-yellow-600">
           Shop now
         </button>
       </div>
       <!-- Image Section -->
-      <div>
-        <img :src="banner" alt="Banner-image" class="w-full h-96 object-cover rounded-md shadow-lg" />
+      <div class="w-1/2 flex gap-6">
+        <!-- <img :src="banna" alt="Banner-image-2" class="w-5/6 h-auto object-cover rounded-sm shadow-lg" /> -->
+        <img :src="banner" alt="Banner-image" class="w-5/6 h-[28rem] object-cover rounded-sm shadow-lg" />
+        
       </div>
     </div>
   </section>
